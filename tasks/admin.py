868a29dbb529c8986d3be9dbc41c617f4fb11d15
Raju Ahmed <rajur20m@gmail.com>
2026-04-03 01:47:10 +0600

    User creation foem

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from tasks.models import Task,Employee,TaskDetail,Project
 
 # Register your models here.
+admin.site.register(Task)
+admin.site.register(TaskDetail)
+admin.site.register(Employee)
+admin.site.register(Project)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def sign_up(request):
+    if request.method=='GET':
+        form= UserCreationForm()
+
+    if request.method=="POST":
+        form= UserCreationForm(request.POST)
+        form.is_valid()
+        form.save()
+
+    return render(request,'registration/register.html',{"form":form})
