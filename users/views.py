3c9fb60b1a8d7815fef3121157e43b6f56fcf92f
Raju Ahmed <rajur20m@gmail.com>
2026-04-03 14:06:47 +0600

    user form extends

--- users/views.py
+++ users/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from users.forms import RegisterForm
 
 # Create your views here.
 def sign_up(request):
     if request.method=='GET':
-        form= UserCreationForm()
+        form= RegisterForm()
 
     if request.method=="POST":
-        form= UserCreationForm(request.POST)
-        form.is_valid()
-        form.save()
+        form= RegisterForm(request.POST)
+        if form.is_valid():
+            # username=form.cleaned_data.get('username')
+            # password=form.cleaned_data.get('password1')
+            # confirm_password=form.cleaned_data.get('password2')
+            # if password==confirm_password:
+            #     User.objects.create(username=username,password=password)
+            # else:
+            #     print('password are not same')
+            form.save()
 
     return render(request,'registration/register.html',{"form":form})
